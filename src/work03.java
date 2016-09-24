d871388067cd37aa8710fbe9b0f41468205410a1
a298582548 <105021010@live.asia.edu.tw>
2016-09-24 21:37:25 +0800

    小小專案第一次構想
    人生第一次想打簡單構想:全自動邊緣人專用，終極密碼!!!!

---
+++ src/work03.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Random;
+public class work03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int a = scn.nextInt();
+		Random rnd = new Random ();
+		int b = rnd.nextInt(10)+1;
+		int c = rnd.nextInt(10)+1;
+		System.out.print(b+("\t")+c);
+		
+		
+		
+    
+	
+		
+
+	}
+
+}
